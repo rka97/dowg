e4dbe02c7cee8a51880daeb181b2905e81b22cf4
Ahmed Khaled <akregeb@gmail.com>
2023-06-05 16:59:13 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="dowg",
-    version="0.1",
+    version="0.12",
     packages=find_packages(),
     install_requires=[
         "torch",
